4c3c6404594278dc248ffff7972195b997b0be84
Koen Vervloesem <koen@vervloesem.eu>
2019-04-19 18:40:33 +0200

    Support DisableApp and EnableApp intents

--- action-supervisor.py
+++ action-supervisor.py
@@ -5,13 +5,17 @@ and restart Snips services.
 """
 
 import importlib
+from pathlib import Path
 from subprocess import run, CalledProcessError
 from threading import Timer
+
+import yaml
 
 from snipskit.apps import SnipsAppMixin
 from snipskit.config import AppConfig
 from snipskit.hermes.apps import HermesSnipsApp
 from snipskit.hermes.decorators import intent, intent_not_recognized
+from sniptkit.mqtt.client import publish_single
 
 # Use the assistant's language.
 i18n = importlib.import_module('translations.' + SnipsAppMixin().assistant['language'])
@@ -19,6 +23,8 @@ i18n = importlib.import_module('translations.' + SnipsAppMixin().assistant['lang
 DELAY_SHUTDOWN = 5
 DELAY_GREET = 5
 DELAY_RESTART_SERVICE = 5
+
+INJECTION_PERFORM = 'hermes/injection/perform'
 
 
 class Supervisor(HermesSnipsApp):
@@ -40,6 +46,11 @@ class Supervisor(HermesSnipsApp):
         except KeyError:  # The user hasn't asked for a reboot.
             pass
 
+        # Inject the names of the installed apps.
+        apps = list(self.app_directories().keys())
+        injections = {'operations': [['add', {'koan/snips-app': apps}]]}
+        publish_single(self.snips.mqtt, INJECTION_PERFORM, injections)
+
     @intent_not_recognized
     def handle_intent_not_recognized(self, hermes, intent_message):
         """Handle unrecognized intents in our dialogue.
@@ -51,8 +62,61 @@ class Supervisor(HermesSnipsApp):
             hermes.publish_end_session(intent_message.session_id,
                                        i18n.RESULT_OK)
 
-    # Restart service
+    def app_directories(self):
+        """Get a dict of the installed apps and their directory."""
+        apps = [app.name for app in (Path(self.assistant.filename).parent / 'snippets').iterdir()]
 
+        try:
+            skills_dir = Path(self.snips['user_dir']) / 'skills'
+        except KeyError:
+            skills_dir = Path('/var/lib/snips/skills')
+
+        with (Path(self.assistant.filename).parent / 'Snipsfile.yaml').open('rt') as snips_file:
+            skill_repos = yaml.safe_load(snips_file)['skills']
+
+        app_repo_names = {item['name']: Path(item['url']).name for item in skill_repos}
+
+        app_dirs = {}
+        for app in apps:
+            app_name = app[app.find('.')+1:].replace('_', ' ')
+            if app in app_repo_names:
+                app_dirs[app_name] = skills_dir / app_repo_names[app]
+            else:
+                app_dirs[app_name] = skills_dir / app
+
+        return app_dirs
+
+    def chmod_app(self, hermes, intent_message, result_sentence, mode):
+        """Change the permissions of an app."""
+        if intent_message.slots and intent_message.slots.app:
+            app = intent_message.slots.app.first().value
+            hermes.publish_end_session(intent_message.session_id,
+                                       result_sentence.format(app))
+            app_dir = self.app_directories()[app]
+            for action in list(app_dir.glob('action-*')):
+                try:
+                    action.chmod(mode)
+                except CalledProcessError:
+                    hermes.publish_end_session(intent_message.session_id,
+                                               i18n.RESULT_SORRY_PERMISSIONS)
+            restart_service_command = self.config['global']['restart_service_command']
+            Timer(DELAY_RESTART_SERVICE,
+                  self.restart_service,
+                  [intent_message.site_id, restart_service_command, 'snips-skill-server']).start()
+
+    # Disable app
+    @intent(i18n.INTENT_DISABLE_APP)
+    def handle_disable_app(self, hermes, intent_message):
+        """Handle the intent DisableApp."""
+        self.chmod_app(hermes, intent_message, i18n.RESULT_DISABLE_APP, 0o644)
+
+    # Enable app
+    @intent(i18n.INTENT_ENABLE_APP)
+    def handle_enable_app(self, hermes, intent_message):
+        """Handle the intent EnableApp."""
+        self.chmod_app(hermes, intent_message, i18n.RESULT_ENABLE_APP, 0o755)
+
+    # Restart service
     @intent(i18n.INTENT_RESTART_SERVICE)
     def handle_restart_service(self, hermes, intent_message):
         """Handle the intent RestartService."""
@@ -88,27 +152,30 @@ class Supervisor(HermesSnipsApp):
         except CalledProcessError:
             self.hermes.publish_start_session_notification(site_id, i18n.RESULT_SORRY, '')
 
-    # Reboot
+    def confirm_and_do(self, intent_message, confirm_option, result_sentence, confirm_intent, custom_data, function):
+        """Ask confirmation and do something."""
+        confirmation_needed = self.config.getboolean('global',
+                                                     confirm_option,
+                                                     fallback=True)
+        if confirmation_needed:
+            self.hermes.publish_continue_session(intent_message.session_id,
+                                                 result_sentence,
+                                                 [confirm_intent],
+                                                 custom_data=custom_data,
+                                                 send_intent_not_recognized=True)
+        else:
+            self.function(intent_message)
 
+    # Reboot
     @intent(i18n.INTENT_REBOOT)
     def handle_reboot(self, hermes, intent_message):
         """Handle the intent Reboot."""
-        try:
-            confirmation_needed = self.config.getboolean('global',
-                                                         'reboot_confirm',
-                                                         fallback=True)
-        except ValueError:
-            confirmation_needed = True
-
-        if confirmation_needed:
-            result_sentence = i18n.RESULT_CONFIRM_REBOOT
-            hermes.publish_continue_session(intent_message.session_id,
-                                            result_sentence,
-                                            [i18n.INTENT_CONFIRM_REBOOT],
-                                            custom_data='Reboot',
-                                            send_intent_not_recognized=True)
-        else:
-            self.announce_and_reboot(intent_message)
+        self.confirm_and_do(intent_message,
+                            'reboot_confirm',
+                            i18n.RESULT_CONFIRM_REBOOT,
+                            i18n.INTENT_CONFIRM_REBOOT,
+                            'Reboot',
+                            self.announce_and_reboot)
 
     @intent(i18n.INTENT_CONFIRM_REBOOT)
     def handle_confirm_reboot(self, hermes, intent_message):
@@ -136,26 +203,15 @@ class Supervisor(HermesSnipsApp):
             self.config.write()
 
     # Shutdown
-
     @intent(i18n.INTENT_SHUTDOWN)
     def handle_shutdown(self, hermes, intent_message):
         """Handle the intent Shutdown."""
-        try:
-            confirmation_needed = self.config.getboolean('global',
-                                                         'shutdown_confirm',
-                                                         fallback=True)
-        except ValueError:
-            confirmation_needed = True
-
-        if confirmation_needed:
-            result_sentence = i18n.RESULT_CONFIRM_SHUTDOWN
-            hermes.publish_continue_session(intent_message.session_id,
-                                            result_sentence,
-                                            [i18n.INTENT_CONFIRM_SHUTDOWN],
-                                            custom_data='Shutdown',
-                                            send_intent_not_recognized=True)
-        else:
-            self.announce_and_shutdown(intent_message)
+        self.confirm_and_do(intent_message,
+                            'shutdown_confirm',
+                            i18n.RESULT_CONFIRM_SHUTDOWN,
+                            i18n.INTENT_CONFIRM_SHUTDOWN,
+                            'Shutdown',
+                            self.announce_and_shutdown)
 
     @intent(i18n.INTENT_CONFIRM_SHUTDOWN)
     def handle_confirm_shutdown(self, hermes, intent_message):
--- translations/en.py
+++ translations/en.py
@@ -7,16 +7,21 @@ of the Supervisor app.
 RESULT_BACK = "Hi, I'm back!"
 RESULT_CONFIRM_REBOOT = "Do you really want to reboot the system?"
 RESULT_CONFIRM_SHUTDOWN = "Do you really want to shutdown the system?"
+RESULT_DISABLE_APP = "OK, I'll disable the {} app and restart all Snips actions."
+RESULT_ENABLE_APP = "OK, I'll enable the {} app and restart all Snips actions."
 RESULT_OK = "OK"
 RESULT_REBOOT = "OK, I'll reboot now. I'll be back."
 RESULT_RESTART_SERVICE = "OK, I'll restart the {} service."
 RESULT_RESTART_ALL_SERVICES = "OK, I'll restart all Snips services."
 RESULT_SHUTDOWN = "OK, I'll shutdown now. Please wake me up soon."
 RESULT_SORRY = "Sorry, I encountered an error. Have a look at the logs to solve it."
+RESULT_SORRY_PERMISSIONS = "Sorry, I couldn't change the execute permissions of the action files."
 
 # Intents
 INTENT_CONFIRM_REBOOT = 'koan:ConfirmReboot'
 INTENT_CONFIRM_SHUTDOWN = 'koan:ConfirmShutdown'
+INTENT_DISABLE_APP = 'koan:DisableApp'
+INTENT_ENABLE_APP = 'koan:EnableApp'
 INTENT_REBOOT = 'koan:Reboot'
 INTENT_RESTART_SERVICE = 'koan:RestartService'
 INTENT_SHUTDOWN = 'koan:Shutdown'
